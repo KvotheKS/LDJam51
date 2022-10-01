f93fd8a8b93ec198379debc3d17cb8c186ca236c
Mateus paula rodrigues <mateus_p_r@hotmail>
2022-10-01 20:54:53 -0300

    um monte de bragalho sei la

--- GodotProject/Scripts/Player.gd
+++ GodotProject/Scripts/Player.gd
@@ -20,12 +20,15 @@ func _process(delta: float) -> void:
 func PlayerInputs():
 	velocity.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	velocity.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	
+	if(Input.is_action_pressed("w_key")):
+		$Sprite/DamageTakenBehavior.Start()
 	return
 
 func OnFall():	
-	$Sprite.modulate = Color(1, 0, 0, 1)
+	#$Sprite.modulate = Color(1, 0, 0, 1)
 	return
 
 func OnEnter():
-	$Sprite.modulate = Color.white
+	#$Sprite.modulate = Color.white
 	return
